7162d2abdaa2558db15989642c4b3eb7b79cffc0
rizaozer <rizaoozer@gmail.com>
2022-07-05 14:30:04 +0300

    delete unused anotations

--- order-management-system/src/main/java/com/example/ordermanagementsystem/config/JpaConfig.java
+++ order-management-system/src/main/java/com/example/ordermanagementsystem/config/JpaConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.example.ordermanagementsystem.dao")
-@PropertySource("classpath:/application.properties")
 public class JpaConfig {
 
 }
